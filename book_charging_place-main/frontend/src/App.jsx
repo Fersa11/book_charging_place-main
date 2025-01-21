160f0033422c44d1afbe7f6c62d4140c6a724e14
Fersa11 <fersa@gmx.net>
2025-01-21 15:24:52 +0100

    250121_setTime_flackert

--- book_charging_place-main/frontend/src/App.jsx
+++ book_charging_place-main/frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer/Footer";
 import Body_Header from "./components/Body/Body_Header";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Clock from "./Features/Clock";
+// import Clock from "./Features/Clock";
 
 // import AnalogClock from "./components/Navigation/Analog";
 
--- book_charging_place-main/frontend/src/pages/HomePage.jsx
+++ book_charging_place-main/frontend/src/pages/HomePage.jsx
@@ -6,11 +6,11 @@ function HomePage() {
   return (
     <div className="Appbody">
       <Card slotName="5A" id="5A" name="5A" />
-      <Card slotName="5B" id="5B" name="5B" />
+      {/* <Card slotName="5B" id="5B" name="5B" />
       <Card slotName="6A" id="6A" name="6A" />
       <Card slotName="6B" id="6B" name="6B" />
       <Card slotName="7A" id="7A" name="7A" />
-      <Card slotName="7B" id="7B" name="7B" />
+      <Card slotName="7B" id="7B" name="7B" /> */}
     </div>
   );
 }
